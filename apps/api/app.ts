91aee83daeb54b804657868caf35e0108d422285
yokesh-ks <ksyokesh98@gmail.com>
2026-04-13 16:12:15 +0530

    feat: Monorepo intialized - frontend and backend

---
+++ apps/api/app.ts
@@ -0,0 +1,40 @@
+import createHttpError, { HttpError } from 'http-errors';
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req: Request, res: Response, next: NextFunction) {
+  next(createHttpError(404));
+});
+
+// error handler
+app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // return JSON error response
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    ...(process.env.NODE_ENV === 'development' && { error: err })
+  });
+});
+
+export default app;
---
+++ docs/phase-0.md
@@ -0,0 +1,26 @@
+# Deliverables
+
+- Monorepo / Workspace Setup
+- Docker Compose for Local Infra
+- API Service Scaffold (Node.js + TypeScript)
+- Worker Service Scaffold (Queue / Background Processing)
+- Frontend Dashboard Scaffold (React)
+- PostgreSQL Container
+- Redis Container
+- Shared Types / Contracts Package
+- Database Package / ORM Setup
+- Initial Documentation Structure
+
+
+## 1. Monorepo Setup
+
+```
+pnpm dlx create-turbo@latest
+```
+
+```
+pnpm create vite
+```
+
+
+
